1c817dd68587a85a625aaeb4f387747c24359b67
EcoLaec <157534603+EcoLaec@users.noreply.github.com>
2024-03-21 10:51:25 -0500

    Removed complicated features

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -20,10 +20,16 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sprite = $AnimatedSprite2D
 @onready var coyote_time = $CoyoteTime
+@onready var wall_time = $WallTime
 var air_jumps_made = 0
 var just_wall_jumped = false
+var stored_wall_normal = Vector2.ZERO
 var clinging = false
 var sprinting = false
+var respawn_point = Vector2.ZERO
+
+func _ready():
+	respawn_point = global_position
 
 func _physics_process(delta):
 	var input_axis = Input.get_axis("Left","Right")
@@ -36,11 +42,15 @@ func _physics_process(delta):
 	update_animations(input_axis)
 	# Before Moving
 	var was_on_floor = is_on_floor()
+	var was_on_wall = is_on_wall_only()
+	if was_on_wall: stored_wall_normal = get_wall_normal()
 	move_and_slide()
 	# After Moving
 	just_wall_jumped = false
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge: coyote_time.start()
+	var just_left_wall = was_on_wall and not is_on_wall()
+	if just_left_wall: wall_time.start()
 
 func apply_gravity(delta):
 	var current_terminal_velocity = cling_speed if clinging else terminal_velocity
@@ -50,12 +60,13 @@ func apply_gravity(delta):
 
 func handle_wall_jump():
 	# Skip if not on Wall
-	if not is_on_wall_only(): return
-	var wall_normal = get_wall_normal()
+	if not is_on_wall_only() and wall_time.time_left <= 0.0: return
+	var wall_normal = stored_wall_normal if wall_time.time_left > 0.0 else get_wall_normal()
 	if Input.is_action_just_pressed("Jump"):
 		velocity.x = wall_normal.x * speed * wall_jump_horizontal_scale
 		velocity.y = jump_velocity * wall_jump_vertical_scale
 		just_wall_jumped = true
+		air_jumps_made = 0
 
 func handle_jump():
 	# Resetting Air Jumps
@@ -63,7 +74,6 @@ func handle_jump():
 	# Jumping
 	if (is_on_floor() or coyote_time.time_left > 0.0) and Input.is_action_just_pressed("Jump"):
 		velocity.y = jump_velocity
-	
 	# Off of Floor
 	if not is_on_floor():
 		# Short Hops
@@ -99,7 +109,7 @@ func update_animations(input_axis):
 	# Grounded Animations
 	if input_axis:
 		sprite.flip_h = input_axis < 0
-		if sprinting: 
+		if sprinting:
 			sprite.play("sprint")
 		else:
 			sprite.play("walk")
@@ -113,3 +123,9 @@ func update_animations(input_axis):
 		else:
 			sprite.play("fall")
 		if clinging: sprite.play("cling")
+
+func die():
+	global_position = respawn_point
+
+func _on_hazard_collider_body_entered(_body):
+	die()
